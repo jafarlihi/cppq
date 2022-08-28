fd858234d05aa4d466fe86a11bba6d7571d10ad4
Hikmat Jafarli <jafarlihi@gmail.com>
2022-08-29 01:48:59 +0400

    Add stuff

--- main.cpp
+++ main.cpp
@@ -43,6 +43,15 @@ std::string stateToString(TaskState state) {
   return "Unknown";
 }
 
+TaskState stringToState(std::string state) {
+  if (state.compare("Unknown") == 0) return TaskState::Unknown;
+  if (state.compare("Pending") == 0) return TaskState::Pending;
+  if (state.compare("Active") == 0) return TaskState::Active;
+  if (state.compare("Failed") == 0) return TaskState::Failed;
+  if (state.compare("Completed") == 0) return TaskState::Completed;
+  return TaskState::Unknown;
+}
+
 std::string uuidToString(uuid_t uuid) {
   char uuid_str[37];
   uuid_unparse_lower(uuid, uuid_str);
@@ -55,11 +64,23 @@ class Task {
       uuid_generate(this->uuid);
       this->type = type;
       this->payload = payload;
+      this->state = TaskState::Unknown;
       this->maxRetry = maxRetry;
       this->timeoutMs = timeoutMs;
       this->retried = 0;
       this->dequeuedAtMs = 0;
-      this->state = TaskState::Unknown;
+    }
+    Task(std::string uuid, std::string type, std::string payload, std::string state, uint64_t maxRetry, uint64_t timeoutMs, uint64_t retried, uint64_t dequeuedAtMs) {
+      uuid_t uuid_parsed;
+      uuid_parse(uuid.c_str(), uuid_parsed);
+      uuid_copy(this->uuid, uuid_parsed);
+      this->type = type;
+      this->payload = nlohmann::json::parse(payload);
+      this->maxRetry = maxRetry;
+      this->timeoutMs = timeoutMs;
+      this->retried = retried;
+      this->dequeuedAtMs = dequeuedAtMs;
+      this->state = stringToState(state);
     }
     uuid_t uuid;
     std::string type;
@@ -104,6 +125,41 @@ void enqueue(redisContext *c, std::shared_ptr<Task> task) {
     throw std::runtime_error("Failed to enqueue task");
 }
 
+std::shared_ptr<Task> dequeue(redisContext *c) {
+  redisReply *reply = (redisReply *)redisCommand(c, "LRANGE cppq:pending -1 -1)");
+  if (reply->type != REDIS_REPLY_ARRAY)
+    throw std::runtime_error("Failed to dequeue task");
+  if (reply->elements == 0)
+    return nullptr;
+  reply = reply->element[0];
+  std::string uuid = reply->str;
+
+  redisCommand(c, "MULTI");
+  redisCommand(c, "LREM cppq:pending 1 %s", uuid.c_str());
+  redisCommand(c, "HGET cppq:task:%s type", uuid.c_str());
+  redisCommand(c, "HGET cppq:task:%s payload", uuid.c_str());
+  redisCommand(c, "HGET cppq:task:%s state", uuid.c_str());
+  redisCommand(c, "HGET cppq:task:%s maxRetry", uuid.c_str());
+  redisCommand(c, "HGET cppq:task:%s timeoutMs", uuid.c_str());
+  redisCommand(c, "HGET cppq:task:%s retried", uuid.c_str());
+  redisCommand(c, "HGET cppq:task:%s dequeuedAtMs", uuid.c_str());
+  redisCommand(c, "LPUSH cppq:active %s", uuid.c_str());
+  reply = (redisReply *)redisCommand(c, "EXEC");
+
+  if (reply->type != REDIS_REPLY_ARRAY || reply->elements != 9)
+    throw std::runtime_error("Failed to dequeue task");
+
+  std::shared_ptr<Task> task = std::make_shared<Task>(uuid, reply->element[1]->str, reply->element[2]->str, reply->element[3]->str, reply->element[4]->integer, reply->element[5]->integer, reply->element[6]->integer, reply->element[7]->integer);
+
+  assert(task->state == TaskState::Pending);
+
+  return task;
+}
+
+void runServer(redisContext *c) {
+
+}
+
 int main(int argc, char *argv[]) {
   handlers[TypeEmailDelivery] = &HandleEmailDeliveryTask;
   handlers[TypeImageResize] = &HandleImageResizeTask;
@@ -121,4 +177,6 @@ int main(int argc, char *argv[]) {
 
   std::shared_ptr<Task> task = NewEmailDeliveryTask(EmailDeliveryPayload{.UserID = 666, .TemplateID = "AH"});
   enqueue(c, task);
+
+  runServer(c);
 }
